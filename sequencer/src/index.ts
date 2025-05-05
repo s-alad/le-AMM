d861474e51614ef6b4634aeae57e697c775b365d
onkr0d <90716666+onkr0d@users.noreply.github.com>
2025-05-05 00:15:38 -0400

    fix: sequencer close after use

--- sequencer/src/index.ts
+++ sequencer/src/index.ts
@@ -7,7 +7,7 @@ import crypto from 'crypto';
 import { getPublicKey } from "@noble/secp256k1";
 import { pubToAddress } from "./cryptography/decryption.js";
 import { VsockServer, VsockSocket } from 'node-vsock';
-import { getAttestationDoc, open } from 'aws-nitro-enclaves-nsm-node';
+import { getAttestationDoc, open, close } from 'aws-nitro-enclaves-nsm-node';
 import { decryptEciesEnvelope, EncryptedEnvelope } from "./cryptography/decryption.js";
 import { SwapRequest } from "./cryptography/constants.js";
 
@@ -22,18 +22,20 @@ const seqpubhex = "0x" + Buffer.from(
 // generate an attestation document
 function attest(nonce?: Buffer): Buffer {
   console.log("[SEQ] generating attestation document");
+  let fd = open(); 
   try {
-    let fd = open();
     let attestDoc = getAttestationDoc(
       fd,
       null,
       nonce, // Use the provided nonce
       Buffer.from(seqpubhex.substring(2), 'hex')
     );
+    close(fd);
     return attestDoc;
   } catch (e) {
     console.error("[SEQ] error generating attestation document:", e);
     console.log("error:", e);
+    close(fd);
     throw e;
   }
 }
